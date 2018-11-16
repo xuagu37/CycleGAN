25d6cf85fe9cc05044bb49058796530c3910ed8f
Xuan Gu <xuagu37@aprilia.imt.liu.se>
2018-11-16 19:34:21 +0100

    a test run

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 
 from CycleGAN import *
 
-trainA_dir = '/home/xuagu37/CycleGAN/T1_subject_1_1000_slice_66.nii.gz'
-trainB_dir = '/home/xuagu37/CycleGAN/FA_subject_1_1000_slice_66.nii.gz'
+trainA_dir = '/home/xuagu37/CycleGAN/data/T1_subject_1_1000_slice_66.nii.gz'
+trainB_dir = '/home/xuagu37/CycleGAN/data/FA_subject_1_1000_slice_66.nii.gz'
 models_dir = '/home/xuagu37/CycleGAN/models'
 batch_size = 10
 

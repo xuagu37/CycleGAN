9898d512b99fe73086c9d8ec6d3d650da39f6aa6
Xuan Gu <xuagu37@aprilia.imt.liu.se>
2018-11-16 20:24:30 +0100

    a test run

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from CycleGAN import *
 trainA_dir = '/home/xuagu37/CycleGAN/data/T1_subject_1_1000_slice_66.nii.gz'
 trainB_dir = '/home/xuagu37/CycleGAN/data/FA_subject_1_1000_slice_66.nii.gz'
 models_dir = '/home/xuagu37/CycleGAN/models'
-batch_size = 10
+batch_size = 20
 
 myCycleGAN = CycleGAN(trainA_dir, trainB_dir, models_dir, batch_size)
 myCycleGAN.train()

d600530c08f2d2b8990c1c5519cbe1d327135160
Xuan Gu <guxuan@msn.com>
2019-09-25 17:32:07 +0200

    Update main.py

--- code/main.py
+++ code/main.py
@@ -5,10 +5,10 @@ from CycleGAN import *
 # Create a CycleGAN on GPU 0
 myCycleGAN = CycleGAN(0)
 
-trainA_dir = '/mnt/wd12t/CycleGAN/HCP/data/T1_subject_1_1000_slice_66.nii.gz'
-trainB_dir = '/mnt/wd12t/CycleGAN/HCP/data/FA_subject_1_1000_slice_66.nii.gz'
-models_dir = '/mnt/wd12t/CycleGAN/HCP/train_subject_1_1000_slice_66_T1_FA_us/models'
-output_sample_dir = '/mnt/wd12t/CycleGAN/HCP/train_subject_1001_1065_slice_66_T1_FA_us/output_sample.png'
+trainA_dir = '/home/xuagu37/CycleGAN/data/T1_training.nii.gz'
+trainB_dir = '/home/xuagu37/CycleGAN/data/FA_training.nii.gz'
+models_dir = '/home/xuagu37/CycleGAN/train_T1_FA/models'
+output_sample_dir = '/home/xuagu37/CycleGAN/train_T1_FA/output_sample.png'
 batch_size = 10
 epochs = 200
 normalization_factor_A = 1000
@@ -16,10 +16,10 @@ normalization_factor_B = 1
 myCycleGAN.train(trainA_dir, normalization_factor_A, trainB_dir, normalization_factor_B, models_dir, batch_size, epochs, output_sample_dir=output_sample_dir, output_sample_channels=1)
 
 for epoch in range(20, 201, 20):
-    G_X2Y_dir = '/mnt/wd12t/CycleGAN/HCP/train_subject_1_1000_slice_66_T1_FA_us/models/G_A2B_weights_epoch_' + str(epoch) + '.hdf5'
+    G_X2Y_dir = '/home/xuagu37/CycleGAN/train_T1_FA/models/G_A2B_weights_epoch_' + str(epoch) + '.hdf5'
     print(G_X2Y_dir)
-    test_X_dir = '/mnt/wd12t/CycleGAN/HCP/data/T1_subject_1001_1065_slice_66.nii.gz'
-    synthetic_Y_dir = '/mnt/wd12t/CycleGAN/HCP/train_subject_1_1000_slice_66_T1_FA_us/synthetic/FA_synthetic_train_subject_1_1000_slice_66_test_subject_1001_1065_slice_66_epoch_' + str(epoch) + '.nii.gz'
+    test_X_dir = '/home/xuagu37/CycleGAN/data/T1_test.nii.gz'
+    synthetic_Y_dir = '/home/xuagu37/CycleGAN/train_T1_FA/synthetic/FA_synthetic_epoch_' + str(epoch) + '.nii.gz'
     normalization_factor_X = 1000
     normalization_factor_Y = 1
     myCycleGAN.synthesize(G_X2Y_dir, test_X_dir, normalization_factor_X, synthetic_Y_dir, normalization_factor_Y)
